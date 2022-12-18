ad17c28706b7712d4172357b2720fbac0eb153a6
iLugroup Multimedia <ilugroupdotcom@gmail.com>
2022-12-18 14:47:28 +0800

    renew payroll index

--- payroll/layouts/sidebar-new.php
+++ payroll/layouts/sidebar-new.php
@@ -62,7 +62,7 @@
 									
 										<ul class="kt-menu__subnav">
 											<li class="kt-menu__item" aria-haspopup="true"><a href="gaji-pegawai-bulanan.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Tetap</span></a></li>
-											<li class="kt-menu__item" aria-haspopup="true"><a href="gaji-pegawai-bulanan.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Kontrak Bulanan</span></a></li>
+											<li class="kt-menu__item" aria-haspopup="true"><a href="gaji-pegawai-kontrak-bulanan.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Kontrak Bulanan</span></a></li>
 											<li class="kt-menu__item" aria-haspopup="true"><a href="gaji-pegawai-harian.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Kontrak Harian</span></a></li>
 										</ul>
 									</div>
