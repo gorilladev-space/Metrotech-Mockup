b6fbcc4062c631448369a2d48ecf8c43931467c2
iLugroup Multimedia <ilugroupdotcom@gmail.com>
2023-01-03 01:53:19 +0800

    payroll harian

--- payroll/layouts/sidebar-new.php
+++ payroll/layouts/sidebar-new.php
@@ -81,7 +81,16 @@
 																</ul>
 															</div>
 														</li>
-											<li class="kt-menu__item" aria-haspopup="true"><a href="gaji-pegawai-harian.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Kontrak Harian</span></a></li>
+											<li class="kt-menu__item  kt-menu__item--submenu" aria-haspopup="true" data-ktmenu-submenu-toggle="hover"><a href="javascript:;" class="kt-menu__link kt-menu__toggle"><i class="kt-menu__link-bullet kt-menu__link-bullet--line"><span></span></i><span class="kt-menu__link-text">Pegawai Kontrak Harian</span><i class="kt-menu__ver-arrow la la-angle-right"></i></a>
+															<div class="kt-menu__submenu "><span class="kt-menu__arrow"></span>
+																<ul class="kt-menu__subnav">
+																	<li class="kt-menu__item " aria-haspopup="true"><a href="gaji-pegawai-harian-add.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Setup Gaji Kontrak Harian</span></a></li>
+																	<li class="kt-menu__item " aria-haspopup="true"><a href="data-gaji-pegawai-harian.php" class="kt-menu__link "><i class="kt-menu__link-bullet kt-menu__link-bullet--dot"><span></span></i><span class="kt-menu__link-text">Data Gaji Kontrak Harian</span></a></li>
+																	
+																	
+																</ul>
+															</div>
+														</li>
 										</ul>
 									</div>
 								</li>
