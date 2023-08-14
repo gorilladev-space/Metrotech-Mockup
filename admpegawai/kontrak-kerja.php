7249139dd86553e7cc7828da32f4e173e6c0e281
iLugroup Multimedia <ilugroupdotcom@gmail.com>
2023-08-14 23:21:04 +0800

    .
    .

--- admpegawai/kontrak-kerja.php
+++ admpegawai/kontrak-kerja.php
@@ -230,13 +230,19 @@
 																<div class="dropdown-menu dropdown-menu-right" x-placement="bottom-end" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-149px, 33px, 0px);">
 																	<ul class="kt-nav">
 																	
-																	
-<li class="kt-nav__item">
+																		<li class="kt-nav__item">
+																			<a href="pegawai-kontrak-detail.php" class="kt-nav__link">
+																				<i class="kt-nav__link-icon fa fa-eye"></i>
+																				<span class="kt-nav__link-text">Lihat Detail Pegawai</span>
+																			</a>
+																		</li>
+																		<li class="kt-nav__item">
 																			<a href="hr-perjanjian-kontrak-add.php" class="kt-nav__link">
 																				<i class="kt-nav__link-icon flaticon-file-2"></i>
 																				<span class="kt-nav__link-text">Buat Perjanjian Kontrak</span>
 																			</a>
 																		</li>
+																		
 																		
 																		
 
